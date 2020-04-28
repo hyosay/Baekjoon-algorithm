8de1ac3f9c88397aa55068ff3ffa6631f79fc3c0
hyoseong-j <gytjdwjs@cu.ac.kr>
2020-04-28 19:00:37 +0900

    2020.04.28

--- level_6/level_6_4_3052.py
+++ level_6/level_6_4_3052.py
@@ -1,9 +1,8 @@
 number_remainder = []
 number_count = 0
+a = 0
 for i in range(10):
     number = int(input())
     number_remainder.append(number % 42)
-list_remainder = str(number_remainder)
-for j in list_remainder:
-    number_count = number_count + number_remainder.count(str(j))
-print(number_count)
+number_remainder = set(number_remainder)
+print(len(number_remainder))
