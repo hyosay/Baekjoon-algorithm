1882379bf52c03e8952d6c9a0d486d40f0608819
hyoseong-j <gytjdwjs@cu.ac.kr>
2020-07-28 19:58:57 +0900

    2020.07.28

--- level_11/level_11_4_11729.py
+++ level_11/level_11_4_11729.py
@@ -17,9 +17,10 @@ N = int(input())
 hanoi_count(N)
 hanoi(N,1,3,2)'''
 
-def h(n, a, b, c):
-    if n == 1:print(a, c);return
-    h(n-1,a, c, b);print(a, c);h(n-1,b,a,c)
-k = int(input())
-print(2 ** k - 1)
-h(k,1,2,3)
+def h(n,a,b,c):
+    if n == 1:
+        print(a, c);return
+    h(n -1,a,c,b)
+    print(a, c)
+    h(n- 1,b,a,c)
+h(3,1,2,3)
