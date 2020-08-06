cdff4293753525cf823e6b96399ba5b1b77b8001
hyoseong-j <gytjdwjs@cu.ac.kr>
2020-08-06 23:11:56 +0900

    2020.08.06 minus에 대한 최빈값

--- level_14/level_14_4_2108.py
+++ level_14/level_14_4_2108.py
@@ -1,28 +1,48 @@
-
-N = int(input())
-N = [int(input()) for i in range(0, N)]
+import sys
+N = int(sys.stdin.readline())
+N = [int(sys.stdin.readline()) for i in range(0, N)]
 def n1(N):
-    return sum(N) / len(N)
+    return round(sum(N) / len(N))
 def n2(N):
     sor = sorted(N, reverse= False)
     midd = int((len(N) / 2))
     return sor[midd]
 def n3(N):
-    mx = max(N)
-    array = [0] * mx
+
     for i in N:
-        array[i - 1] += 1
-    if array.index(max(array)) == 0:
-        return 1
+        if i < 0:
+            mi = [0] * (max(N) + abs(min(N)) + 1)
+            break
+        else:
+            mi = [0] * (max(N) + 1)
+
+    kill = len(mi) - max(N) - 1
+    for i in N:
+        if i > 0:
+            mi[i + kill] += 1
+        else:
+            mi[i + kill] += 1
+
+    maxmi = max(mi)
+    temp = maxmi
+    a = []
+    #이 부분 다시 설정
+    for k in range(-kill, len(mi) - kill):
+        if temp <= mi[k + kill]:
+            temp = mi[k + kill]
+            a.append(k)
+    print(mi)
+    if len(a) <= 2:
+        return a[0]
     else:
-        return array.index(max(array))
+        return a[1]
 
+def n4(N):
+    return max(N) - min(N)
 
+print(n1(N))
+print(n2(N))
 print(n3(N))
-
-
-
-
-
+print(n4(N))
 
 
--- sort.py
+++ sort.py
@@ -1,31 +1,32 @@
-import math
-math
-array = [1,3,8, 2, -2]
-mi = [0] * (max(array) + 1) #0 ~  max
-if min(array) < 0:
-    minus = [0] * (abs(min(array)))
-print(mi)
-print(minus)
+array = [1,3,8, 2, -2, -1,]
 
-'''
+for i in range(0, len(array)):
+    if array[i] < 0:
+        mi = [0] * (max(array) + abs(min(array)) + 1)
+    else:
+        mi = [0] * (max(array) + 1) #0 ~  max
+
+kill = len(mi) - max(array) - 1
+
 for i in array:
     if i > 0:
-        mi[i] += 1
-        print(mi)
+        mi[i + kill] += 1
+    else:
+        mi[i + kill] += 1
 
+print(mi)
 
 maxmi = max(mi)
 temp = maxmi
 a = []
-for k in range(0, len(mi)):
-    if temp <= mi[k]:
-         temp = mi[k]
+
+for k in range(-kill, len(mi) - kill):
+    if temp <= mi[k + kill]:
+         temp = mi[k + kill]
          a.append(k)
+
 print(a)
 if len(a) <= 2:
     print(a[0])
 else:
-    print(a[1])'''
-
-
-
+    print(a[1])
