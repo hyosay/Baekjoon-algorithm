f9550f1097393eab34de8d6f7e8eae9c3018221b
hyoseong-j <gytjdwjs@cu.ac.kr>
2020-08-17 18:55:08 +0900

    2020/08/17 level_18_3_11399.py ATM

--- level_18/level_18_3_11399.py
+++ level_18/level_18_3_11399.py
@@ -1,7 +1,11 @@
 N = int(input())
 people = sorted(list(map(int, input().split())))
-
 time = 0
-for i in people:
-    time += i + time
+for i in range(0, N):
+    if len(people) == N:
+        time += sum(people[0:i + 1])
 print(time)
+
+
+# 숏코딩
+print(sum(sum(people[:i + 1]) for i in range(N)))
